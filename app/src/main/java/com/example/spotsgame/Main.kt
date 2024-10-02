8b262408101c13b6aa3dd265ada13133fe92a116
Elnur Kasimov <ekasimov2012@gmail.com>
2024-10-02 13:06:41 +0300

    Create displayPlayingField() and it works proper

--- app/src/main/java/com/example/spotsgame/Main.kt
+++ app/src/main/java/com/example/spotsgame/Main.kt
@@ -1,5 +1,31 @@
 package com.example.spotsgame
 
-fun main() {
+import kotlin.random.Random
 
+fun main() {
+    println("Welcome to the Spot Game")
+    val playingChips = Array(16){ it + 1 }
+    playingChips.shuffle()
+    do {
+        displayPlayingField(playingChips)
+        print("Enter number You want to move or \"Q\" for quit : ")
+        val step = readln()
+        val numberForMove: Int?  = step.toIntOrNull()
+    } while (numberForMove != null)
+    println(
+        """Thank You for playing!
+           |GoodLuck!""".trimMargin())
+}
+
+fun displayPlayingField(playingChips: Array<Int>) {
+    println("----------------------")
+    for (i in 0 until 4) {
+        print("|   ")
+        for(j in 0 until 4) {
+            print(if (playingChips[i * 4 + j] == 16) "  \t"
+                  else "${playingChips[i * 4 + j]}\t" )
+            }
+        println(" |")
+    }
+    println("----------------------")
 }
